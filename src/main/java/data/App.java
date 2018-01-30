e67414683cca390ec54083ddebd5c5d866b433dc
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-30 11:42:06 +0800

    遍历数据

--- src/main/java/data/App.java
+++ src/main/java/data/App.java
@@ -2,6 +2,7 @@ package data;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static data.DBcon.getConn;
@@ -12,10 +13,12 @@ import static data.DBcon.getConn;
 public class App {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = getConn("root", "123456", "test");  //没有密码就什么都不写，空字符串
-		PreparedStatement pst = conn.prepareStatement("INSERT INTO Testing(Id) VALUES(?)");
-		for (int i = 1; i <= 1000; i++) {
-			pst.setInt(1, i * 2);
-			pst.executeUpdate();
+		PreparedStatement pst = conn.prepareStatement("SELECT * FROM Authors");
+		ResultSet rs = pst.executeQuery();
+		while (rs.next()) {
+			System.out.print(rs.getInt(1));
+			System.out.print(": ");
+			System.out.println(rs.getString(2));
 		}
 		pst.close();//显示关闭Statement对象，释放资源
 		conn.close();// 关闭数据库连接，这是个好习惯。尽管在程序运行结束会自动关闭。但web应用是不会结束运行的。
