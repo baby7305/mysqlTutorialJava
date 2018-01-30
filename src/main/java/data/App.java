f50f1e30b26d1adc81d4d80acc4ffb4c3279b87e
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-30 11:32:20 +0800

    插入1000条数据

--- src/main/java/data/App.java
+++ src/main/java/data/App.java
@@ -1,23 +1,23 @@
 package data;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static data.DBcon.getConn;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main(String[] args) throws SQLException {
-        Connection conn = getConn("root", "123456", "test");  //没有密码就什么都不写，空字符串
-        PreparedStatement pst = conn.prepareStatement("INSERT INTO Authors(Name) VALUES(?)");
-        pst.setString(1, "author");
-        pst.executeUpdate();
-        pst.close();//显示关闭Statement对象，释放资源
-        conn.close();// 关闭数据库连接，这是个好习惯。尽管在程序运行结束会自动关闭。但web应用是不会结束运行的。
-    }
+public class App {
+	public static void main(String[] args) throws SQLException {
+		Connection conn = getConn("root", "123456", "test");  //没有密码就什么都不写，空字符串
+		Statement stmt = conn.createStatement();
+		for (int i = 1; i <= 1000; i++) {
+			String query = "INSERT INTO Testing(Id) VALUES(" + 2 * i + ")";
+			stmt.executeUpdate(query);
+		}
+		stmt.close();//显示关闭Statement对象，释放资源
+		conn.close();// 关闭数据库连接，这是个好习惯。尽管在程序运行结束会自动关闭。但web应用是不会结束运行的。
+	}
 }
