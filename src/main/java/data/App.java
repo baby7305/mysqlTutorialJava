6092c086755c9039f551fc1b2463ab71d4673029
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-30 11:35:38 +0800

    预插入1000条数据

--- src/main/java/data/App.java
+++ src/main/java/data/App.java
@@ -1,8 +1,8 @@
 package data;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import static data.DBcon.getConn;
 
@@ -12,12 +12,12 @@ import static data.DBcon.getConn;
 public class App {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = getConn("root", "123456", "test");  //没有密码就什么都不写，空字符串
-		Statement stmt = conn.createStatement();
+		PreparedStatement pst = conn.prepareStatement("INSERT INTO Testing(Id) VALUES(?)");
 		for (int i = 1; i <= 1000; i++) {
-			String query = "INSERT INTO Testing(Id) VALUES(" + 2 * i + ")";
-			stmt.executeUpdate(query);
+			pst.setInt(1, i * 2);
+			pst.executeUpdate();
 		}
-		stmt.close();//显示关闭Statement对象，释放资源
+		pst.close();//显示关闭Statement对象，释放资源
 		conn.close();// 关闭数据库连接，这是个好习惯。尽管在程序运行结束会自动关闭。但web应用是不会结束运行的。
 	}
 }
